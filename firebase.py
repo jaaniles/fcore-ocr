edba5293e06939acdb32b951c3423aaa4359e443
jaaniles <jaani.leskinen@fraktio.fi>
2024-10-23 21:39:44 +0300

    feat: add loading firebase config from firebase_config.json

--- firebase.py
+++ firebase.py
@@ -1,18 +1,16 @@
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 import pyrebase
 
+def load_config():
+    with open('firebase_config.json') as config_file:
+        return json.load(config_file)
+
 # Initialize Pyrebase for Firebase Authentication (Client SDK)
-def initialize_pyrebase():
-    firebase_config = {
-        "apiKey": "",
-        "authDomain": "",
-        "projectId": "",
-        "storageBucket": "",
-        "messagingSenderId": "",
-        "appId": "",
-        "databaseURL": ""
-    }
+def initialize_pyrebase(config):
+    firebase_config = config['firebase']
+
     firebase = pyrebase.initialize_app(firebase_config)
     return firebase
 
@@ -21,8 +19,10 @@ def initialize_firebase_admin():
     firebase_admin.initialize_app(cred)
     return firestore.client()
 
+firebase_config = load_config()
+
 # Initialize Firestore and Auth clients
 initialize_firebase_admin()
-firebase = initialize_pyrebase()
+firebase = initialize_pyrebase(firebase_config)
 auth = firebase.auth()
 db = firestore.client()
